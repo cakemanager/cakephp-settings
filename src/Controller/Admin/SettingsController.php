fbb7c8174e3103bf18ecfd03ffca8dea0fd79651
Bob <bobmulder96@gmail.com>
2015-02-26 00:15:35 +0100

    removed unnecessary actions

--- src/Controller/Admin/SettingsController.php
+++ src/Controller/Admin/SettingsController.php
@@ -53,6 +53,7 @@ class SettingsController extends AppController
         if ($this->request->is(['patch', 'post', 'put'])) {
             $settings = $this->Configurations->patchEntities($settings, $this->request->data);
             foreach ($settings as $setting) {
+
                 $this->Flash->success('The settings has been saved.');
 
                 if (!$this->Configurations->save($setting)) {
@@ -63,79 +64,6 @@ class SettingsController extends AppController
         }
 
         $this->set(compact('prefix', 'settings'));
-    }
-
-    /**
-     * View method
-     *
-     * @param string|null $id Setting id
-     * @return void
-     * @throws \Cake\Network\Exception\NotFoundException
-     */
-    public function view($id = null) {
-        $setting = $this->Configurations->get($id, [
-            'contain' => []
-        ]);
-        $this->set('setting', $setting);
-    }
-
-    /**
-     * Add method
-     *
-     * @return void
-     */
-    public function add() {
-        $setting = $this->Configurations->newEntity($this->request->data);
-        if ($this->request->is('post')) {
-            if ($this->Configurations->save($setting)) {
-                $this->Flash->success('The setting has been saved.');
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error('The setting could not be saved. Please, try again.');
-            }
-        }
-        $this->set(compact('setting'));
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Setting id
-     * @return void
-     * @throws \Cake\Network\Exception\NotFoundException
-     */
-    public function edit($id = null) {
-        $setting = $this->Configurations->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $setting = $this->Configurations->patchEntity($setting, $this->request->data);
-            if ($this->Settings->save($setting)) {
-                $this->Flash->success('The setting has been saved.');
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error('The setting could not be saved. Please, try again.');
-            }
-        }
-        $this->set(compact('setting'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Setting id
-     * @return void
-     * @throws \Cake\Network\Exception\NotFoundException
-     */
-    public function delete($id = null) {
-        $this->request->allowMethod(['post', 'delete']);
-        $setting = $this->Configurations->get($id);
-        if ($this->Configurations->delete($setting)) {
-            $this->Flash->success('The setting has been deleted.');
-        } else {
-            $this->Flash->error('The setting could not be deleted. Please, try again.');
-        }
-        return $this->redirect(['action' => 'index']);
     }
 
     /**
