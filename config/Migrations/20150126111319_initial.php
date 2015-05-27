2a7cd6b1baf8bb050ba200d726c5eff910aca84e
BobMulder <bobmulder@outlook.com>
2015-05-27 08:50:06 +0200

    Migration update.

--- config/Migrations/20150126111319_initial.php
+++ config/Migrations/20150126111319_initial.php
@@ -1,5 +1,17 @@
 <?php
-
+/**
+ * CakeManager (http://cakemanager.org)
+ * Copyright (c) http://cakemanager.org
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) http://cakemanager.org
+ * @link          http://cakemanager.org CakeManager Project
+ * @since         1.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 use Phinx\Migration\AbstractMigration;
 
 class Initial extends AbstractMigration
@@ -13,50 +25,46 @@ class Initial extends AbstractMigration
     public function up() {
         $table = $this->table('settings_configurations');
         $table
-                ->addColumn('name', 'string', [
-                    'limit'   => '100',
-                    'null'    => '',
-                    'default' => '',
-                ])
-                ->addColumn('value', 'text', [
-                    'limit'   => '',
-                    'null'    => '',
-                    'default' => '',
-                ])
-                ->addColumn('description', 'text', [
-                    'limit'   => '',
-                    'null'    => '',
-                    'default' => '',
-                ])
-                ->addColumn('type', 'string', [
-                    'limit'   => '50',
-                    'null'    => '',
-                    'default' => '',
-                ])
-                ->addColumn('editable', 'integer', [
-                    'limit'   => '11',
-                    'null'    => '',
-                    'default' => '1',
-                ])
-                ->addColumn('options', 'text', [
-                    'limit'   => '',
-                    'null'    => '',
-                    'default' => '',
-                ])
-                ->addColumn('weight', 'integer', [
-                    'limit'   => '11',
-                    'null'    => '',
-                    'default' => '0',
-                ])
-                ->addColumn('created', 'datetime', [
-                    'limit'   => '',
-                    'null'    => '',
-                ])
-                ->addColumn('modified', 'datetime', [
-                    'limit'   => '',
-                    'null'    => '',
-                ])
-                ->create();
+            ->addColumn('name', 'string', [
+                'limit'   => '100',
+                'default' => '',
+            ])
+            ->addColumn('value', 'text', [
+                'default' => '',
+                'null' => true
+            ])
+            ->addColumn('description', 'text', [
+                'default' => '',
+                'null' => true
+            ])
+            ->addColumn('type', 'string', [
+                'limit'   => '50',
+                'default' => '',
+            ])
+            ->addColumn('editable', 'integer', [
+                'limit'   => '11',
+                'default' => '1',
+            ])
+            ->addColumn('options', 'text', [
+                'limit'   => '',
+                'null'    => true,
+                'default' => '',
+            ])
+            ->addColumn('weight', 'integer', [
+                'limit'   => '11',
+                'default' => '0',
+            ])
+            ->addColumn('autoload', 'integer', [
+                'limit'   => '11',
+                'default' => '1',
+            ])
+            ->addColumn('created', 'datetime', [
+                'null'    => '',
+            ])
+            ->addColumn('modified', 'datetime', [
+                'null'    => '',
+            ])
+            ->create();
     }
 
     /**
